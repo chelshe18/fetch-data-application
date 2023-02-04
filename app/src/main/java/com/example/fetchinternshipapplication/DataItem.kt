6d05698925cff660079cad9639b9b72d153b54e1
chelshe18 <chehechen@gmail.com>
2023-02-04 09:38:12 -0600

    Implemented Comparable interface.

--- app/src/main/java/com/example/fetchinternshipapplication/DataItem.kt
+++ app/src/main/java/com/example/fetchinternshipapplication/DataItem.kt
@@ -1,7 +1,14 @@
 package com.example.fetchinternshipapplication
 
-data class DataItem (
-        val id: String,
-        val listId: String,
+data class DataItem(
+        val id: Int,
+        val listId: Int,
         val name: String?
-        )
+        ) : Comparable<DataItem> {
+
+        override operator fun compareTo(other: DataItem) = when (listId) {
+            other.listId -> name!!.drop(5).toInt() - other.name!!.drop(5).toInt()
+            else -> listId - other.listId
+        }
+
+        }
